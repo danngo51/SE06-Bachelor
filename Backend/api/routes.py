d1863d6a3520e39f4b41a8520b40f4dd963b06c6
Dan Ngo <danngo51@gmail.com>
2025-04-03 13:51:36 +0200

    project structure

--- Backend/api/routes.py
+++ Backend/api/routes.py
@@ -1,7 +1,10 @@
 from fastapi import APIRouter
-from api import api  # Import API submodule
+from Backend.api.api import api  # Import API submodule
+from Backend.api.predict import predict  # Import Predict submodule
+
 
 router = APIRouter()
 
 # Register API routes from `api.py`
 router.include_router(api.router, prefix="/api", tags=["API"])
+router.include_router(predict.router , prefix="/predict", tags=["Predict"])
